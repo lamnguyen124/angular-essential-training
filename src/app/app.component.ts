e7a41817c50793169b2c1eb3686ca545cae829da
Lam <nguyen.h.lam12@gmail.com>
2019-10-30 15:09:45 -0500

    progressing with exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  firstMediaItem = {
+    id: 1,
+    name: 'Firebug',
+    medium: 'Series',
+    category: 'Science Fiction',
+    year: 2010,
+    watchedOn: null,
+    isFavorite: false
+  };
+
+  onMediaItemDelete(mediaItem) {
+    
+  }
+}
